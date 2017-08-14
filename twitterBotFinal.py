1805e19768094424ff0b4f0d18680ef4ffe93b76
Pallavi Dutta <pallaviduttadgp@gmail.com>
2017-08-14 19:28:48 +0530

    Updated

--- twitterBotFinal.py
+++ twitterBotFinal.py
@@ -16,14 +16,14 @@ def setTwitterAuth():
     api = ty.API(auth)
     return api
 
-#This method gets the last 100 tweets from the user's timelinereturns a list of tweet objects.
+#This method gets the last 100 tweets from the user's timeline and returns a list of tweet objects.
 def getTimeline(api, user):
     tweets = api.user_timeline(user.screen_name, count=100)
     tweets = [tweet for tweet in tweets]
     print(tweets[0])
     return tweets
 
-
+#This method gets the most recent tweet.
 def getLastTweet(api, user):
     api.update_status("My last tweet is as"
                       " follows...#{}".format(random.randint(0, 10000)))
@@ -38,14 +38,14 @@ def getLastTweet(api, user):
         else:
             tweet = tweets[0]
             api.update_status(tweet[:14] + "...wait, my last tweet was about"
-                              " tweeting my last tweet, do'oh! #{}".
+                              " tweeting my last tweet! #{}".
                               format(random.randint(0, 10000)))
     except ty.RateLimitError:
         print("You've hit the API limit! Try your bot in about an hour.")
     except ty.TweepError as e:
         print("You've hit another error. This could be a lot of things, but {}".format(e))
 
-
+#This function gets 100 search results of the string search, and returns them as a list of tweet objects.
 def searchTweet(api, searchTerm):
     searchResults = [status for status in ty.
                      Cursor(api.search, q=searchTerm).items(100)]
@@ -53,6 +53,7 @@ def searchTweet(api, searchTerm):
     likedStatus = api.create_favorite(searchTweetID)
     api.update_status("Liked tweet with #IOT!".format(random.randint(0, 10000)))
 
+#This likes a tweet once it is tweeted by the bot user.
 def likeTweetOfUser(api,boltuser,user) :
     tweet = api.user_timeline(boltuser, count=1)
     tweetID=tweet[0].id
@@ -61,20 +62,13 @@ def likeTweetOfUser(api,boltuser,user) :
 
 
 
-def replyHelloWorld(api, searchResults):
-    randomTweet = searchResults[random.randint(0, len(searchResults) - 1)]
-    tweet = ("@{} This is a demo search for 'hello world' with a botT, hello"
-             " world! #{}".format(randomTweet.user.screen_name,
-                                  random.randint(0, 10000)))
-    tweetID = randomTweet.id
-    api.update_status(tweet, tweetID)
 
 
+#The followers and follow methods help us to get the no of people we have as followers and the no of people we follow.
 if __name__ == "__main__":
     api = setTwitterAuth()
     user = api.me()
     print(user.screen_name)
-
     api.update_status("I have {} followers and follow {} accounts #{} LATESTT 2!!!"
                       .format(user.followers_count, user.friends_count,
                               random.randint(0, 10000)))
@@ -84,5 +78,5 @@ if __name__ == "__main__":
     search = "#IOT"
     searchTweet(api, search)
 
-
+#This likes any tweet that has the hashtag '#IOT'
     likeTweetOfUser(api, "@boltiot", user)
